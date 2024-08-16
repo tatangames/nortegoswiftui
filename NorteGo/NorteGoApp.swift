d457c674988d816cd51aac871e5a54a9ee9d2812
Jonathan Moran <tatangamess@gmail.com>
2024-08-16 17:36:34 -0600

    Initial Commit

---
+++ NorteGo/NorteGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NorteGoApp.swift
+//  NorteGo
+//
+//  Created by Jonathan  Moran on 16/8/24.
+//
+
+import SwiftUI
+
+@main
+struct NorteGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
